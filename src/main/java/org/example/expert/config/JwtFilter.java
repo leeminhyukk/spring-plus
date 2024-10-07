39531d703ec31197132e12569c53bd4443b92be8
lklll1212 <lklll1212@gmail.com>
2024-10-07 17:06:23 +0900

    사용하지 않는 임폴트 정리. 테스트에 닉네임 추가된거 반영.

--- src/main/java/org/example/expert/config/JwtFilter.java
+++ src/main/java/org/example/expert/config/JwtFilter.java
@@ -11,7 +11,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.user.enums.UserRole;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
